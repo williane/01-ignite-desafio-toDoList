e9c418b95555ee1e8b8d5aeaea798426e7f43acf
Williane Marques (DHL Supply Chain) <Williane.Marques@dhl.com>
2023-03-31 12:55:02 -0300

    add the logic to done taks

--- src/components/tasks/TasksList/TaskDetail/index.tsx
+++ src/components/tasks/TasksList/TaskDetail/index.tsx
@@ -1,11 +1,20 @@
 import Trash from "../../../../assets/trash.svg";
+import Check from "../../../../assets/check.svg";
 import styles from "./styles.module.css";
 
 export function TaskDetail() {
+  const isDone = false;
+
   return (
     <div className={styles.task}>
-      <button className={styles.check}></button>
-      <p>
+      {isDone ? (
+        <button className={styles.checked}>
+          <img src={Check} alt="icone de checked" />
+        </button>
+      ) : (
+        <button className={styles.check}></button>
+      )}
+      <p className={isDone ? styles.done : ""}>
         Integer urna interdum massa libero auctor neque turpis turpis semper.
         Duis vel sed fames integer.
       </p>
